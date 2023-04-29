ac1fca9d1dffbbffdcc8218b90c15a8b124951d5
Fernando Aceves <FAAceves@gmail.com>
2023-04-29 14:16:35 -0300

    fixed undefined runningSites

--- dist/autoclaim-v3-beta.user.js
+++ dist/autoclaim-v3-beta.user.js
@@ -2,7 +2,7 @@
 // @name         [satology] Auto Claim Multiple Faucets with Monitor UI
 // @description  Automatic rolls and claims for 50+ crypto faucets/PTC/miners (Freebitco.in BTC, auto promo code for 16 CryptosFaucet, FaucetPay, StormGain, etc)
 // @description  Claim free ADA, BNB, BCH, BTC, DASH, DGB, DOGE, ETH, FEY, LINK, LTC, NEO, SHIB, STEAM, TRX, USDC, USDT, XEM, XRP, ZEC, ETC
-// @version      3.0.0
+// @version      3.0.1
 // @author       satology
 // @namespace    satology.onrender.com
 // @homepage     https://criptologico.com/tools/cc
@@ -584,7 +584,7 @@
                 }
             };
             function loadFlowControl() {
-                runningSites = persistence.load('runningSites', true);
+                runningSites = persistence.load('runningSites', true) || {};
             };
             function setFlowControl(schedule, id, url, webType, params = null) {
                 runningSites[schedule] = {
