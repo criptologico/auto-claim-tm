6c2b358d5d0100e22ca4cc91d434073f9d2fc71e
DanielTori <danith1986@gmail.com>
2023-08-29 21:06:18 +0200

    update description

--- src/meta/description.js
+++ src/meta/description.js
@@ -1,2 +1,2 @@
-// @description  Automatic rolls and claims for 50+ crypto faucets/PTC/miners (Freebitco.in BTC, auto promo code for 16 CryptosFaucet, FaucetPay, StormGain, etc)
+// @description  Automatic rolls and claims for 50+ crypto faucets/PTC/miners (Freebitco.in BTC, auto promo code for 20 CryptosFaucet, FaucetPay, StormGain, etc)
 // @description  Claim free ADA, BNB, BCH, BTC, DASH, DGB, DOGE, ETH, FEY, LINK, LTC, NEO, SHIB, STEAM, TRX, USDC, USDT, XEM, XRP, ZEC, ETC
